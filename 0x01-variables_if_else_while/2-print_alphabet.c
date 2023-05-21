869d54547aa7d28c133749042e0e7da1816ae27d
kansiimeneverce <kansiimeneverce@gmail.com>
2023-05-21 14:22:25 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ */
+int main(void)
+{
+
+	char alpha;
+
+	for (alpha = 'a'; alpha < '>'; alpha++)
+	{
+		putchar(alpha);
+	}
+	putchar('\n');
+
+	return (0);
+}	

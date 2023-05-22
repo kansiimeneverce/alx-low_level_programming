424ad0e5f64120874243494b616163572db004d7
kansiimeneverce <kansiimeneverce@gmail.com>
2023-05-22 12:35:26 -0700

    the numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - print numbers form base 0 to 9
+ * Return: always 0
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 48; a <= 57; a++)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}

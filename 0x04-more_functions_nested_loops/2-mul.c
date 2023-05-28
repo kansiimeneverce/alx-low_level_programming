47acea20c68694c260a394ee35b000ff15251b1f
kansiimeneverce <kansiimeneverce@gmail.com>
2023-05-28 05:59:06 -0700

    ./2-mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two inters.
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: product of @a * @b
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+

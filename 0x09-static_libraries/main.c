1e8acff6fef370435f371872c430eda3997b4d11
kansiimeneverce <kansiimeneverce@gmail.com>
2023-06-11 11:24:18 -0700

    libmy.a, main.h

--- 0x09-static_libraries/main.c
+++ 0x09-static_libraries/main.c
@@ -4,4 +4,24 @@ int main(void)
 {
     _puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kansiime Neverce");
     return (0);
-}
+}	    
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+	   

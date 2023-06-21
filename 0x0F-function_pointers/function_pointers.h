479763660f018c37703c73651aa87221f3eb2309
kansiimeneverce <kansiimeneverce@gmail.com>
2023-06-21 06:36:47 -0700

    0-print_name.c

---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,22 @@
+#ifndef FUCTIONPOINTERS_FUNCTION_POINTERS_H
+#define FUCTIONPOINTERS_FUNCTION_POINTERS_H
+
+typedef int (*operation_ptr) (int, int);
+
+// Add function
+int add(int a, int b);
+
+// Multiply function
+int multiply(int a, int b);
+
+// Function Pointer:
+int do_operation(int (*op)(int, int), int x, int y);
+
+// Function Pointer:
+int do_ptr(operation_ptr op_ptr, int x, int y);
+
+// Test function:
+uint32_t do_test(uint32_t (*op) (uint32_t, char[9]), uint32_t octet, char hex[9]);
+uint32_t test(uint32_t octet, char hex[9]);
+
+#endif

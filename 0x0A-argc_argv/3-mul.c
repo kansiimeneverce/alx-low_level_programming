14ebda25f415aecb60220e832beb9a4f636e6449
kansiimeneverce <kansiimeneverce@gmail.com>
2023-06-25 04:16:30 -0700

    3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,46 +1,28 @@
 #include <stdio.h>
-#include "main.h"
 
 /**
- * _atoi - converts a string to an integer
- * @s: string to be converted
+ * main - Entry point
  *
- * Return: the int converted from the string
+ * @argc: incoming argument
+ * @argv: incoming argument
+ *
+ * Return: Always 0 (Success)
  */
-int _atoi(char *s)
-{
-	int i, d, n, len, f, digit;
-
-	i = 0;
-	d = 0;
-	n = 0;
-	len = 0;
-	f = 0;
-	digit = 0;
-
-	while (s[len] != '\0')
-		len++;
-
-	while (i < len && f == 0)
+int main(int argc, char **argv)
 	{
-		if (s[i] == '-')
-			++d;
 
-		if (s[i] >= '0' && s[i] <= '9')
-			digit = s[i] - '0';
-			if (d % 2)
-				digit = -digit;
-			n = n * 10 + digit;
-			f = 1;
-			if (s[i + 1] < '0' || s[i + 1] > '9')
-				break;
-			f = 0;
-		}
-		i++;
+	 int total;
+
+	if (argc < 2)
+	{
+		printf("Error\n");
+		return (1);
 	}
 
-	if (f == 0)
-		return (0);
+	total = (int) *argv[1] * (int) *argv[2];
 
-	return (n);
-}
+	printf("%d\n", total);
+
+	return (0);
+
+	}
